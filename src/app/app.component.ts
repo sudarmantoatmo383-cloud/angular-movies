5c797d99aa12766ac22736c12e2a9e66688af17f
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-19 21:51:02 +0700

    23. Pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,17 @@
+import { DatePipe, UpperCasePipe, CurrencyPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   movie = {
     title: 'Spider-Man',
-    releaseDate: new Date()
+    releaseDate: new Date(),
+    price: 1400.99
   }
 }
