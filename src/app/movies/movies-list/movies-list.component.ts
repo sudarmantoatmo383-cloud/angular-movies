01afd0dc7c5f28de840f7a1b846d7a758f8316c1
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-20 05:37:17 +0700

    35. Installing Angular Material

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -1,10 +1,12 @@
 import { DatePipe, UpperCasePipe, CurrencyPipe } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { GenericListComponent } from '../../shared/components/generic-list/generic-list.component';
+import { MatButtonModule } from "@angular/material/button";
+import { MatIconModule } from "@angular/material/icon";
 
 @Component({
   selector: 'app-movies-list',
-  imports: [DatePipe, UpperCasePipe, CurrencyPipe, GenericListComponent],
+  imports: [DatePipe, UpperCasePipe, CurrencyPipe, GenericListComponent, MatButtonModule, MatIconModule],
   templateUrl: './movies-list.component.html',
   styleUrl: './movies-list.component.css'
 })
