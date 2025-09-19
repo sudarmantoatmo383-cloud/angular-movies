52b2b61f52028175b10697da6f12d7eb68aba2a2
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-20 05:55:49 +0700

    37. Dynamic CSS Classes with NgClass

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MoviesListComponent } from './movies/movies-list/movies-list.component';
 import { MenuComponent } from "./shared/components/menu/menu.component";
+import { RatingComponent } from "./shared/components/rating/rating.component";
 
 @Component({
   selector: 'app-root',
-  imports: [MoviesListComponent, MenuComponent],
+  imports: [MoviesListComponent, MenuComponent, RatingComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/shared/components/rating/rating.component.ts
+++ src/app/shared/components/rating/rating.component.ts
@@ -1,11 +1,41 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-rating',
-  imports: [],
+  imports: [MatIconModule, NgClass],
   templateUrl: './rating.component.html',
   styleUrl: './rating.component.css'
 })
 export class RatingComponent {
+  ngOnInit(): void {
+    this.maxRatingArray = Array(this.maxRating).fill(0);
+  }
+  @Input({required: true})
+  maxRating!: number;
 
+  @Input()
+  selectedRating=0;
+
+  clickedRating = 0;
+
+  maxRatingArray: any[] = []
+
+  handleMouseEnter(index: number){
+    this.selectedRating = index + 1;
+  }
+
+  handleMouseLeave(){
+    if (this.clickedRating !== 0){
+      this.selectedRating = this.clickedRating;
+    }else{
+      this.selectedRating = 0;
+    }
+  }
+
+  handleClick(index: number){
+    this.selectedRating = index + 1;
+    this.clickedRating = this.selectedRating;
+  }
 }
