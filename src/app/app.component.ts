aad3abe26e5705f56252874c464c3878d38d591a
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-19 21:45:15 +0700

    22. Interpolation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,8 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angular-movies';
+  movie = {
+    title: 'Spider-Man',
+    releaseDate: new Date()
+  }
 }
