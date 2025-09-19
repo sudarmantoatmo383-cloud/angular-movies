3594f37563861fb616027fdcc146b5962b14f169
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-19 22:32:56 +0700

    31. Events

--- src/app/movies/movies-list/movies-list.component.ts
+++ src/app/movies/movies-list/movies-list.component.ts
@@ -9,6 +9,19 @@ import { Component, Input } from '@angular/core';
 })
 export class MoviesListComponent {
   @Input({required: true})
-  movies?: any[];
+  movies!: any[];
+
+  addMovies(){
+    this.movies?.push({
+      title: 'Inception',
+      releaseDate: new Date('2012-07-03'),
+      price: 500
+    })
+  }
+
+  removeMovie(movie: any){
+    let index = this.movies.findIndex((m: any) => m.title === movie.title);
+    this.movies.splice(index, 1);
+  }
 
 }
