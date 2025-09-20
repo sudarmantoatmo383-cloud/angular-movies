a8f2de288e8e20d718bddfd2fbbb3db915412d8e
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-20 15:51:03 +0700

    49. Handling Unknown Routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -27,5 +27,7 @@ export const routes: Routes = [
     {path: 'theaters/edit/:id', component: EditTheaterComponent},
 
     {path: 'movies/create', component: CreateMovieComponent},
-    {path: 'movies/edit/:id', component: EditMovieComponent}
+    {path: 'movies/edit/:id', component: EditMovieComponent},
+
+    {path: '**', redirectTo: ''}
 ];
