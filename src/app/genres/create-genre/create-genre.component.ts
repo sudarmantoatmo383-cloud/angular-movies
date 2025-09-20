3ebf29739d5bef02d1332de24bf0be00ff9c6888
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-20 16:06:09 +0700

    53. Creating Our First Form

--- src/app/genres/create-genre/create-genre.component.ts
+++ src/app/genres/create-genre/create-genre.component.ts
@@ -1,20 +1,28 @@
 import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-create-genre',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, ReactiveFormsModule, MatFormFieldModule, MatInputModule, RouterLink],
   templateUrl: './create-genre.component.html',
   styleUrl: './create-genre.component.css'
 })
 export class CreateGenreComponent {
 
   router = inject(Router);
-  
-  saveChanges(){
-    //..save changes
+  private formBuilder = inject(FormBuilder);
 
+  form = this.formBuilder.group({
+    name: ['']
+  })
+
+  saveChange(){
+    // .. save changes
+    console.log(this.form.value)
     this.router.navigate(['/genres']);
   }
 }
