a5cd05b407b637ad0a8186af0e5d1e9608f3d327
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-20 15:32:04 +0700

    47. Navigating with JavaScript (step 2)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,22 @@ import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
 import { CreateGenreComponent } from './genres/create-genre/create-genre.component';
+import { IndexActorsComponent } from './actors/index-actors/index-actors.component';
+import { CreateActorComponent } from './actors/create-actor/create-actor.component';
+import { CreateTheaterComponent } from './theaters/create-theater/create-theater.component';
+import { IndexTheatersComponent } from './theaters/index-theaters/index-theaters.component';
+import { CreateMovieComponent } from './movies/create-movie/create-movie.component';
 
 export const routes: Routes = [
     {path: '', component: LandingPageComponent},
     {path: 'genres', component: IndexGenresComponent},
-    {path: 'genres/create', component: CreateGenreComponent}
+    {path: 'genres/create', component: CreateGenreComponent},
+
+    {path: 'actors', component: IndexActorsComponent},
+    {path: 'actors/create', component: CreateActorComponent},
+
+    {path: 'theaters', component: IndexTheatersComponent},
+    {path: 'theaters/create', component: CreateTheaterComponent},
+
+    {path: 'movies/create', component: CreateMovieComponent}
 ];
