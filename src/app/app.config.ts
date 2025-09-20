93a4ecd8f5da3888508922a1d00be96c9eb66efb
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-20 16:17:53 +0700

    54. Built-In Validation Rules (long error message)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,11 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
+import { MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), 
-    provideRouter(routes, withComponentInputBinding())]
+    provideRouter(routes, withComponentInputBinding()),
+    {provide: MAT_FORM_FIELD_DEFAULT_OPTIONS, useValue: {subscriptSizing: 'dynamic'}}
+  ]
 };
