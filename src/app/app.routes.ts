669c7723649703c53f64306546b3d90070089312
DESKTOP-F5LQ268\darma <sai.team00@gmail.com>
2025-09-20 14:56:58 +0700

    46. Configuring Routing in Our App (step 2)

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
+import { IndexGenresComponent } from './genres/index-genres/index-genres.component';
 
 export const routes: Routes = [
-    {path: '', component: LandingPageComponent}
+    {path: '', component: LandingPageComponent},
+    {path: 'genres', component: IndexGenresComponent}
 ];
---
+++ src/app/genres/index-genres/index-genres.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-index-genres',
+  imports: [],
+  templateUrl: './index-genres.component.html',
+  styleUrl: './index-genres.component.css'
+})
+export class IndexGenresComponent {
+
+}
